a7975370e3e4c42cca7b7348912da995fc9a9adc
Bazzan <bazzanapalowow@gmail.com>
2021-04-16 00:56:24 +0100

    'budgetapp'

--- BudgetApp.py
+++ BudgetApp.py
@@ -20,6 +20,27 @@ class Budget:
 
         clothing_balance: float|int
             Stores the balance of fund allocated for clothing by the user
+
+        HOW IT WORKS:
+       --------------
+            tunde = Budget('Tunde')
+            tunde.deposit(23.12, 'clothing')
+            tunde.deposit(34.16, 'entertainment')
+            tunde.withdraw(14, 'clothing')
+            tunde.transfer(20.13, 'entertainment', 'clothing')
+            tunde.category_balance()
+
+
+            ₦23.12 deposited to clothing category.
+            ₦34.16 deposited to entertainment category.
+            ₦14 withdrawn from clothing category.
+            ₦20.13 withdrawn from entertainment category.
+            ₦20.13 deposited to clothing category.
+            Tunde has the following balances:"
+                        food:   ₦0,
+                        clothing: ₦29.25,
+                        entertainment: ₦14.03
+                    TOTAL: ₦43.28
     """
 
     balance = 0
@@ -46,16 +67,15 @@ class Budget:
                 self.food_balance += amount
             elif category == 'clothing':
                 self.clothing_balance += amount
-            elif category == 'entertainment':
-                self.entertainment_balance += amount
             else:
-                print("Enter one of 'food, clothing or entertainment' as the category")
-                return
+                self.entertainment_balance += amount
 
             print('₦{} deposited to {} category.'.format(amount, category))
-
-        self.balance += amount
-        return
+            self.balance += amount
+            return
+        else:
+            print("Enter one of 'food, clothing or entertainment' as the category")
+            return
 
     def withdraw(self, amount, category):
         """
@@ -71,27 +91,28 @@ class Budget:
         if category in ['food', 'clothing', 'entertainment']:
             if category == 'food':
                 if amount > self.food_balance:
-                    print("Amount too much. Your balance is {}".format(self.food_balance))
+                    print("Amount too much. Your food balance is ₦{}".format(round(self.food_balance, 2)))
                     return False
                 self.food_balance -= amount
             elif category == 'clothing':
                 if amount > self.clothing_balance:
-                    print("Amount too much. Your balance is {}".format(self.clothing_balance))
+                    print("Amount too much. Your clothing balance is ₦{}".format(round(self.clothing_balance, 2)))
                     return False
                 self.clothing_balance -= amount
-            elif category == 'entertainment':
+            else:
                 if amount > self.entertainment_balance:
-                    print("Amount too much. Your balance is {}".format(self.entertainment_balance))
+                    print("Amount too much. Your entertainment balance is"
+                          " ₦{}".format(round(self.entertainment_balance, 2)))
                     return False
                 self.entertainment_balance -= amount
-            else:
-                print("Enter one of 'food, clothing or entertainment' as the category")
-                return
 
-        self.balance -= amount
+            self.balance -= amount
+            print('₦{} withdrawn from {} category.'.format(amount, category))
+            return True
 
-        print('₦{} withdrawn from {} category.'.format(amount, category))
-        return True
+        else:
+            print("Enter one of 'food, clothing or entertainment' as the category")
+            return
 
     def transfer(self, amount, from_category, to_category):
         """
@@ -118,11 +139,11 @@ class Budget:
         A method of the Budget class that is used to compute category balances
         """
         print("""{} has the following balances:"
-                food:   ₦{},
-                clothing: ₦{},
-                entertainment: ₦{}
+            food:   ₦{},
+            clothing: ₦{},
+            entertainment: ₦{}
         TOTAL: ₦{}""".format(self.name or 'No Name',
-                             self.food_balance,
-                             self.clothing_balance,
-                             self.entertainment_balance,
-                             self.balance))
+                             round(self.food_balance, 2),
+                             round(self.clothing_balance, 2),
+                             round(self.entertainment_balance, 2),
+                             round(self.balance, 2)))
